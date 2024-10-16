48525e6a99e2eeae8d36eea7993d70ed7d9e9dae
adam <adam.andersson@sitevision.se>
2024-10-16 13:39:48 +0200

    use isAdmin prop

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,19 @@ import PropTypes from "prop-types";
 import Form from "../Form";
 import Posts from "../Posts";
 
-const App = ({ posts }) => {
+const App = ({ posts, isAdmin }) => {
+  const showPosts = posts?.length > 0 && isAdmin === true;
   return (
     <div className="env-p-around--large">
       <Form />
-      {posts?.length > 0 && <Posts posts={posts} />}
+      {showPosts && <Posts posts={posts} />}
     </div>
   );
 };
 
 App.propTypes = {
   posts: PropTypes.array,
+  isAdmin: PropTypes.bool,
 };
 
 export default App;
